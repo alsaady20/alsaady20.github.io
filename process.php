93aed91fef11476ca7501b2d738ecf319911d58c
Ahmad Alsaady <97070340+alsaady20@users.noreply.github.com>
2023-12-14 11:44:46 -0500

    Add files via upload

--- process.php
+++ process.php
@@ -4,9 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    // You can now process the form data as needed (e.g., send an email, store in a database, etc.)
-    
-    // For demonstration purposes, let's just print the data
+
     echo "Name: $name <br>";
     echo "Email: $email <br>";
     echo "Message: $message <br>";
